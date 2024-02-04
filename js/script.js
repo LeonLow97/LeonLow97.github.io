fafbf17ac9e4a911720e0cbb119a456841a68036
LeonLow97 <lowjiewei@gmail.com>
2024-02-05 00:19:41 +0800

    Added active and open states for navbar in about section

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Toggle between skills and education in about section
+const skillsNavEl = document.querySelector('.about-nav-link-skills');
+const educationNavEl = document.querySelector('.about-nav-link-education');
+const skillsListEl = document.querySelector('.about-nav-skills');
+const educationListEl = document.querySelector('.about-nav-education');
+
+document.addEventListener('DOMContentLoaded', () => {
+  skillsNavEl.classList.add('active');
+  skillsListEl.classList.add('open');
+});
+
+skillsNavEl.addEventListener('click', () => {
+  skillsNavEl.classList.add('active');
+  educationNavEl.classList.remove('active');
+
+  skillsListEl.classList.add('open');
+  educationListEl.classList.remove('open');
+});
+
+educationNavEl.addEventListener('click', () => {
+  educationNavEl.classList.add('active');
+  skillsNavEl.classList.remove('active');
+
+  educationListEl.classList.add('open');
+  skillsListEl.classList.remove('open');
+});
